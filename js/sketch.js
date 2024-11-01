9eeede0d089ba5ec6d1cf1489f26aee0f775d3ac
Nicogomezbeltran <73166385+Nicogomezbeltran@users.noreply.github.com>
2024-11-01 20:24:17 +0100

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -38,7 +38,7 @@ function setup() {
 function draw() {
 //En el draw dibuja la imágen de la habitación y llama a las funciones para dibujar los elementos
   background(220);
-  image(habitacion, 0, 0, 1408, 720);
+  image(habitacion, 0, 0, 1410, 720);
   //Llamada a las acciones de dibujar del canvas
   dibujarCuadro();
   dibujarTexto();
